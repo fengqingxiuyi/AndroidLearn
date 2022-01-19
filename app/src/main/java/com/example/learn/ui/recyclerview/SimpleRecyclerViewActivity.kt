938a855abc49dec1b4b0a9f035d345e3fb2cf166
shenbf <shen.binfeng@yalla.live>
2022-01-19 16:36:56 +0800

    缺省页第一种

--- app/src/main/java/com/example/learn/ui/recyclerview/SimpleRecyclerViewActivity.kt
+++ app/src/main/java/com/example/learn/ui/recyclerview/SimpleRecyclerViewActivity.kt
@@ -1,7 +1,16 @@
 package com.example.learn.ui.recyclerview
 
+import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.common.base.BaseActivity
+import com.example.common.ui.empty.TipViewManager
+import com.example.common.ui.empty.config.ITipViewConfig
 import com.example.common.ui.viewBinding
+import com.example.learn.databinding.SimpleRecyclerviewBinding
+import com.example.ui.adapter.ViewHolder
 
 /**
  * 略
@@ -10,6 +19,48 @@ import com.example.common.ui.viewBinding
  */
 class SimpleRecyclerViewActivity : BaseActivity() {
 
-//  private val binding: SimpleRecyclerViewBinding by viewBinding()
+  private val binding: SimpleRecyclerviewBinding by viewBinding()
+//  private var data = listOf<String>("1","1","1","1","1","1","1","1","1","1","1","1")
+  private var data = listOf<String>()
 
+  private val tipViewManager by lazy {
+    val tipViewManager = TipViewManager(this)
+    tipViewManager.build()
+    tipViewManager
+  }
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(binding.root)
+    initRecycler()
+  }
+
+  private fun initRecycler() {
+    binding.rvSimple.layoutManager = LinearLayoutManager(this)
+    binding.rvSimple.adapter = object : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+      override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val textView = TextView(applicationContext)
+        return object : RecyclerView.ViewHolder(textView) {
+
+        }
+      }
+
+      override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val itemView = holder.itemView as TextView
+        itemView.text = data[position]
+      }
+
+      override fun getItemCount(): Int {
+        return data.size
+      }
+    }
+    if (data.isEmpty()) {
+      tipViewManager.showTipView(ITipViewConfig.INoDataConfig.FAVORITED_ROOM_NO_DATA)
+    }
+  }
+
+  override fun onDestroy() {
+    super.onDestroy()
+    tipViewManager.onDestroy()
+  }
 }
